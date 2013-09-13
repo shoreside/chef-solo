e4a2144891f27afb94a6cc1e6abe83cafbb0c001
Christian Schmidt <christian@bookmetender.com>
2013-09-13 14:21:04 +0200

    Only cleanup root grants file if mysql was set up.

--- cookbooks/bookmetender/recipes/cleanup.rb
+++ cookbooks/bookmetender/recipes/cleanup.rb
@@ -1,6 +1,8 @@
-# remove mysql root grants file
-execute "cleanup-after-mysql-install-privileges" do
-  command "rm #{node['mysql']['grants_path']}"
-  action :nothing
-  subscribes :run, "execute[mysql-install-privileges]", :delayed
+unless node['mysql'].nil?
+  # remove mysql root grants file
+  execute "cleanup-after-mysql-install-privileges" do
+    command "rm #{node['mysql']['grants_path']}"
+    action :nothing
+    subscribes :run, "execute[mysql-install-privileges]", :delayed
+  end
 end
